a87a9b55d7d411d9b55fd73b66760af40af6c46c
Tate Campbell <tjcampbell2@usfca.edu>
2016-02-17 21:18:19 -0800

    Update plays_kNN.R

--- plays_kNN.R
+++ plays_kNN.R
@@ -10,7 +10,7 @@ data.training <- data[ind==1, 1:9]
 data.test <- data[ind==2, 1:9]
 data.trainLabels <- data[ind==1, 10]
 data.testLabels <- data[ind==2, 10]
-data_pred <- knn(train = data.training, test = data.test, cl = data.trainLabels, k=3)
+data_pred <- knn(train = data.training, test = data.test, cl = data.trainLabels, k=5)
 CrossTable(x = data.testLabels, y = data_pred, prop.chisq=FALSE)
 
 accuracy = sum(data_pred == data.testLabels)/length(data_pred)
