1c3865f250d21f743dab2393d781685ab48424b1
Tate Campbell <tjcampbell2@usfca.edu>
2016-02-17 19:37:26 -0800

    Create football_ml.py

---
+++ football_ml.py
@@ -0,0 +1,129 @@
+__author__ = 'tatePro'
+#football_ml.py
+
+from pyspark.mllib.feature import HashingTF, IDF
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.classification import NaiveBayes 
+from pyspark.mllib.classification import SVMWithSGD, SVMModel, LogisticRegressionWithLBFGS, LogisticRegressionModel
+from pyspark.mllib.regression import LabeledPoint
+import numpy as np
+from collections import Counter
+
+def dictionarize(line, columns):
+	'''
+	converts a list into a dict using the list columns as keys
+	'''
+	N = len(columns)
+	line_dict = dict()
+	for i in xrange(N):
+		line_dict[columns[i].encode('utf-8')] = line[i].encode('utf-8')
+	return line_dict
+
+def label_description(line, labels):
+	'''
+	replaces a description with a label in labels if that label is contained in the description 
+	'''
+	for l in labels:
+		if l in line['description']:
+			line['description'] = l
+			break
+	return line
+
+def conv_rush_labs(line, labels):
+	for l in labels:
+		if l in line['description']:
+			line['description'] = 'rush'
+			break
+	return line
+
+def encode_pass_rush(line):
+	'''
+	encodes description labels pass -> 1.0, rush -> 0
+	'''
+	if line['description'] == 'pass':
+		line['description'] = 1.0
+	elif line['description'] == 'rush':
+		line['description'] = 0
+	return line 
+
+def prep_svm_data(line, variables):
+	values = list()
+	for v in variables:
+		try:
+			values.append(float(line[v]))
+		except Exception as e:
+			values.append(float(0))
+	return LabeledPoint(values[0], values[1:])
+
+
+d2008 = sc.textFile('nfl_pbp_data/2008_nfl_pbp_data.csv')
+d2009 = sc.textFile('nfl_pbp_data/2009_nfl_pbp_data.csv')
+d2010 = sc.textFile('nfl_pbp_data/2010_nfl_pbp_data.csv')
+d2011 = sc.textFile('nfl_pbp_data/2011_nfl_pbp_data.csv')
+d2012 = sc.textFile('nfl_pbp_data/2012_nfl_pbp_data.csv')
+
+counts = d2008.count(),d2009.count(),d2010.count(),d2011.count(),d2012.count()
+
+d12_8 = d2012.union(d2011).union(d2010).union(d2009).union(d2008)
+nrows = d12_8.count() - 5
+data = d12_8.map(lambda l: l.split(','))
+names = data.take(1)[0]
+
+data_dicts = data.map(lambda l: dictionarize(l, names))
+
+event_types = ['kicks', 'pass', 'punts', 'sacked', 'extra point', 'field goal', 
+'left end', 'left tackle', 'left guard', 'right end', 'right tackle',
+'right guard', 'up the middle', 'kneels', 'PENALTY', 'FUMBLES', 'scrambles', 
+'spiked', 'BLOCKED', 'RECOVERED', 'rushed', 'punted', 'TOUCHDOWN', 'Touchback',
+'kick', 'kicked', 'SAFETY', 'TWO POINT CONVERSION', 'intercepted'
+'TWO POINT CONVERSION ATTEMPT']
+
+# all of the following event_types correspond to rushing plays: left end, left tackle,
+# left guard, right end, right tackle, right guard, up the middle, rushed. 
+
+# in order to perform classification we'll convert all the rushing play labels into 'rush'
+# and then keep only data points with description equal to 'rush' or 'pass'
+
+rushing_types = ['left end', 'left tackle','left guard', 'right end', 'right tackle',
+ 'right guard', 'up the middle', 'rushed']
+
+data_lab_desc = data_dicts.map(lambda line: label_description(line, event_types))
+just_labels = data_lab_desc.map(lambda line: line['description'])
+label_distribution = Counter(just_labels.collect())
+label_distribution
+
+n_labeled = sum([label_distribution[e] for e in event_types])
+print '%d out of %d (%2.2f percent) descriptions have been successfully labeled' % (n_labeled, nrows, 100*float(n_labeled)/nrows) 
+
+data_converted_rush_labs = data_lab_desc.map(lambda line: conv_rush_labs(line, rushing_types))
+just_converted_labels = data_converted_rush_labs.map(lambda line: line['description'])
+conv_lab_dist = Counter(just_converted_labels.collect())
+conv_lab_dist
+
+##############################################################
+
+# we'll encode 'pass' = 1.0 and 'rush' = 0
+
+new_data = data_converted_rush_labs.filter(lambda line: line['description'] in ['rush', 'pass'])
+
+nfl_data = new_data.map(lambda line: encode_pass_rush(line))
+
+VARS = ['description', 'qtr', 'min', 'down', 'togo', 'ydline', 'defscore', 'offscore']
+
+data1 = nfl_data.map(lambda line: prep_svm_data(line, VARS))
+
+svm_model = SVMWithSGD.train(data1, iterations=2000)
+logreg_model = LogisticRegressionWithLBFGS.train(data1)
+
+# Evaluating the svm model on training data
+labelsAndPreds = data1.map(lambda p: (p.label, svm_model.predict(p.features)))
+trainErr = float(labelsAndPreds.filter(lambda (v, p): v != p).count()) / float(data1.count())
+print("SVM Training Error = " + str(trainErr))
+
+# Evaluate the logistic regression on training data 
+labelsAndPreds_log = data1.map(lambda p: (p.label, logreg_model.predict(p.features)))
+trainErr_log = float(labelsAndPreds_log.filter(lambda (v, p): v != p).count()) / float(data1.count())
+print("Logistic Regression Training Error = " + str(trainErr_log))
+print("SVM Training Error = " + str(trainErr))
+
+logreg_model.weights, logreg_model.intercept
